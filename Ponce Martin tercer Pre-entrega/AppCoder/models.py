0643a52a222a9a1d087b56da442fb647186c3f7a
martinponce12 <144730311+martinponce12@users.noreply.github.com>
2024-04-08 20:16:46 -0300

    Add files via upload

---
+++ Ponce Martin tercer Pre-entrega/AppCoder/models.py
@@ -0,0 +1,20 @@
+from django.db import models
+
+# Create your models here.
+class Curso(models.Model):
+
+    nombre=models.CharField(max_length=40)
+    camada = models.IntegerField()
+
+
+class Estudiante(models.Model):
+    nombre= models.CharField(max_length=30)
+    apellido= models.CharField(max_length=30)
+    email= models.EmailField()
+
+class Profesor(models.Model):
+    nombre= models.CharField(max_length=30)
+    apellido= models.CharField(max_length=30)
+    profesion= models.CharField(max_length=30)
+
+
